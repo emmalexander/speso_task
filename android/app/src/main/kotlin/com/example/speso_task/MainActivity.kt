0dd78b5c390b14e8db78611189ced4f743c87982
emmalexander <32887883+emmalexander@users.noreply.github.com>
2023-10-09 11:32:06 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/speso_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.speso_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/local_message.dart
@@ -0,0 +1,6 @@
+class LocalMessage {
+  String chatId;
+  String _id;
+
+  String get id => _id;
+}
